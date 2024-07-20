da60224977370d29998640065d5e250d725c5e92
sellinios <53009345+sellinios@users.noreply.github.com>
2024-07-20 14:36:59 +0300

    Delete api/serializers directory

--- api/serializers/serializer_contact_message.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from api.models import ContactMessage
-
-class ContactMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactMessage
-        fields = '__all__'
-        read_only_fields = ['user']
